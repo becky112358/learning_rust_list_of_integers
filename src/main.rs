185ffdc945d5002b11150b6b574e718d8bac0928
becky112358 <8b2@protonmail.com>
2020-06-23 19:03:19 +0300

    Compute mean.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::convert::TryInto; 
+
 fn main() {
-    println!("Hello, world!");
+
+    let integers = vec![8, -3, 0, 8, 18, 2, -5, 8, 18, -5, 8, 18, 8];
+
+    let mean = mean(integers);
+
+    println!("Mean: {}", mean);
 }
+
+fn mean(v: Vec<i32>) -> f32 {
+
+    let mut sum : i32 = 0;
+
+    for x in &v {
+        sum += x;
+    }
+
+    let length : i32 = v.len().try_into().unwrap();
+
+    (sum as f32) / (length as f32)
+}
+
